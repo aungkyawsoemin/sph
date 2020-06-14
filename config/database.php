3a96b1f447100eaf141e98f6d1e38bbd52b4c23d
aungkyawsoemin <aungkyawsoemin@gmail.com>
2020-06-14 12:15:34 +0630

    Update TestCase of API and UI

--- config/database.php
+++ config/database.php
@@ -63,6 +63,12 @@ return [
             ]) : [],
         ],
 
+        'phpunit' => [
+            'driver' => 'sqlite',
+            'database' => database_path('database.sqlite'),
+            'prefix' => ''
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
---
+++ tests/Feature/ApiTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class ApiTest extends TestCase
+{
+    use DatabaseMigrations;
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $response = $this->json('GET', '/api/air-pollution-data');
+        $response->assertStatus(200);
+    }
+}
